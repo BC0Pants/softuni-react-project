e0bda0f4e5b509b6bf79f55c86c2f5b6733b4e95
bc0pants <bc0pants@gmail.com>
2025-03-04 19:40:53 +0200

    Added and set up mongodb with mongoose

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 
 dotenv.config();
 const port = process.env.PORT || 8080;
+const db = process.env.DBURI;
+
+try {
+  const uri = `${db}`;
+  await mongoose.connect(uri);
+  console.log("DB connected");
+} catch (error) {
+  console.log("Cannot connect to DB");
+  console.error(error);
+}
 
 const app = express();
 
