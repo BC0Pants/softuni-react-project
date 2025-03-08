d7d51afe46a1c9194549f36e6e5669432c599d5c
bc0pants <bc0pants@gmail.com>
2025-03-08 14:53:40 +0200

    Created login service

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -1,7 +1,33 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import User from "../models/userSchema.js";
+
+dotenv.config();
+const secret = process.env.SECRET;
 
 export default {
   register(userData) {
     return User.create(userData);
+  },
+  async login(email, password) {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("Invalid email or password");
+    }
+
+    const isValid = await bcryptjs.compare(password, user.password);
+    if (!isValid) {
+      throw new Error("Invalid email or password");
+    }
+
+    const payload = {
+      id: user.id,
+      email: user.email,
+    };
+    const token = jwt.sign(payload, secret, { expiresIn: "2h" });
+
+    return token;
   },
 };
