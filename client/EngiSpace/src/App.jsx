112e35a57627742f3b985ca0b529c96afa98f6ba
bc0pants <bc0pants@gmail.com>
2025-04-02 22:46:12 +0300

    Updated the about page

--- client/EngiSpace/src/App.jsx
+++ client/EngiSpace/src/App.jsx
@@ -1,14 +1,19 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Welcome from './components/Welcome';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Register from './components/Register';
-import CategoryPosts from './components/CategoryPosts';
-import CreatePost from './components/CreatePost';
-import PostPage from './components/PostPage';
-import Profile from './components/Profile';
-import { useAuth } from './hooks/useAuth';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import CategoryPosts from "./components/CategoryPosts";
+import CreatePost from "./components/CreatePost";
+import PostPage from "./components/PostPage";
+import Profile from "./components/Profile";
+import { useAuth } from "./hooks/useAuth";
 
 function App() {
   const { isAuthenticated } = useAuth();
@@ -18,27 +23,24 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<Welcome />} />
-        <Route 
-          path="/login" 
-          element={isAuthenticated ? <Navigate to="/" /> : <Login />} 
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/login"
+          element={isAuthenticated ? <Navigate to="/" /> : <Login />}
         />
-        <Route 
-          path="/register" 
-          element={isAuthenticated ? <Navigate to="/" /> : <Register />} 
+        <Route
+          path="/register"
+          element={isAuthenticated ? <Navigate to="/" /> : <Register />}
         />
         <Route path="/category/:flagId" element={<CategoryPosts />} />
-        <Route 
-          path="/category/:flagId/posts/create-post" 
-          element={isAuthenticated ? <CreatePost /> : <Navigate to="/login" />} 
+        <Route
+          path="/category/:flagId/posts/create-post"
+          element={isAuthenticated ? <CreatePost /> : <Navigate to="/login" />}
         />
-        <Route 
-          path="/category/:flagId/posts/:postId" 
-          element={<PostPage />} 
-        />
-        <Route 
-          path="/profile" 
-          element={isAuthenticated ? <Profile /> : <Navigate to="/login" />} 
+        <Route path="/category/:flagId/posts/:postId" element={<PostPage />} />
+        <Route
+          path="/profile"
+          element={isAuthenticated ? <Profile /> : <Navigate to="/login" />}
         />
       </Routes>
     </Router>
